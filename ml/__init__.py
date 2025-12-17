2125549696da590d8acbe2f8ea8c76aa94bfc119
Anna1983-lang <rigikanuta@gmail.com>
2025-12-17 08:54:27 +0200

    Update __init__.py

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,29 +1,7 @@
-"""
-Модуль машинного обучения для интеллектуального сервиса генерации
-и проверки заданий по Python.
-
-Используется в веб-приложении Flask и в standalone-обучении моделей.
-"""
-
+from .train import train_all_models
 from .ml_model import (
     load_local_model,
     predict_local_feedback,
-    retrain_model,
     evaluate_model,
     get_model_stats
 )
-
-from .train import (
-    train_model_v1,
-    train_model_v2
-)
-
-__all__ = [
-    "load_local_model",
-    "predict_local_feedback",
-    "retrain_model",
-    "evaluate_model",
-    "get_model_stats",
-    "train_model_v1",
-    "train_model_v2"
-]
