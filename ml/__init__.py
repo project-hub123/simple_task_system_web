f7d49ccc11c382bde0ec1940348c31224606b34a
Anna1983-lang <rigikanuta@gmail.com>
2025-12-17 09:35:15 +0200

    Update __init__.py

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,7 +1,2 @@
-from .train import train_all_models
-from .ml_model import (
-    load_local_model,
-    predict_local_feedback,
-    evaluate_model,
-    get_model_stats
-)
+# ML package
+# offline training is intentionally NOT imported here
