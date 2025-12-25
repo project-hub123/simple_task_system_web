134d221c0b40500c87ba03de7743d73b9df1d388
Anna1983-lang <rigikanuta@gmail.com>
2025-12-25 08:39:17 +0200

    Update __init__.py

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,10 +1,6 @@
-# ML package
-
 from .ml_model import (
     load_local_model,
     predict_local_feedback,
     evaluate_model,
     get_model_stats
 )
-
-from .task_generator import generate_task
