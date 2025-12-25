dfe9a298bbb873fe618d394ab200718b56d4dd23
Anna1983-lang <rigikanuta@gmail.com>
2025-12-25 08:15:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ml.train import train_all_models
 
+
 def main():
     print("=== START ML SYSTEM ===")
     train_all_models()
     print("=== END ML SYSTEM ===")
+
 
 if __name__ == "__main__":
     main()
