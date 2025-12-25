b8f005fecee70b3dcb9aa33c52c6b67a63fc831a
Anna1983-lang <rigikanuta@gmail.com>
2025-12-25 08:06:48 +0200

    2

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,2 +1,10 @@
 # ML package
-# offline training is intentionally NOT imported here
+
+from .ml_model import (
+    load_local_model,
+    predict_local_feedback,
+    evaluate_model,
+    get_model_stats
+)
+
+from .task_generator import generate_task
