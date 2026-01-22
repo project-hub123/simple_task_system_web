d9f4d048e71ddf9d8f26a4f5324979c87b55bbf4
Anna1983-lang <rigikanuta@gmail.com>
2026-01-22 08:46:21 +0200

    Update model_service.py

--- ml/model_service.py
+++ ml/model_service.py
@@ -1,6 +1,7 @@
 import joblib
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent
 MODEL_DIR = BASE_DIR / "models"
 MODEL_DIR.mkdir(exist_ok=True)
@@ -8,15 +9,15 @@ MODEL_DIR.mkdir(exist_ok=True)
 MODEL_PATH = MODEL_DIR / "model_task_classifier.pkl"
 
 
-def save_model(model_data):
+def save_model(model_data: dict):
     joblib.dump(model_data, MODEL_PATH)
 
 
-def load_model():
+def load_model() -> dict:
     if not MODEL_PATH.exists():
-        raise FileNotFoundError("Файл модели не найден")
+        raise FileNotFoundError("Нейросетевая модель не найдена")
     return joblib.load(MODEL_PATH)
 
 
-def model_exists():
+def model_exists() -> bool:
     return MODEL_PATH.exists()
